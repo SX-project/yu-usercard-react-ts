3d2c51403e426206eb72eedbb3d4bc6925b7e841
Yushiro <yu1220.skktc@gmail.com>
2021-07-04 21:39:55 +0900

    commit

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,27 @@
 import { memo, VFC } from "react";
-import { Center, Wrap, WrapItem, Spinner } from "@chakra-ui/react";
+import { Center, Wrap, WrapItem, Spinner, useDisclosure } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useCallback } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
-import { useEffect } from "react";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
+import { useLoginUser } from "../../hooks/useLoginUser";
 
 export const UserManagement: VFC = memo(() => {
     const { getUsers, users, loading } = useAllUsers();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { onSelectUser, selectedUser } = useSelectUser();
+    const { loginUser } = useLoginUser();
+    console.log(loginUser);
+
     useEffect(() => getUsers(), [])
+
+    const onClickUser = useCallback((id: number) => {
+        onSelectUser({ id, users, onOpen });
+    }, [users, onSelectUser, onOpen])
+
 
     return (
         <>
@@ -20,17 +34,21 @@ export const UserManagement: VFC = memo(() => {
                     {users.map((user) => (
                         <WrapItem key={user.id} mx="auto">
                             <UserCard
+                                id={user.id}
                                 imageUrl="https://source.unsplash.com/random"
                                 userName={user.username}
                                 fullName={user.name}
+                                onClick={onClickUser}
                             />
                         </WrapItem>
                     ))}
                 </Wrap>
             )}
+            <UserDetailModal
+                isOpen={isOpen}
+                onClose={onClose}
+                user={selectedUser}
+            />
         </>
-
-
-
     )
 });
